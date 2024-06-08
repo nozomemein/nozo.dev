a92540f9f511c0a8bbc790872f7cea85ae120b6e
nozomemein <b8yukifsukeo999n@gmail.com>
2024-06-08 10:49:49 +0900

    🎨add link to twitter/github

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,27 @@
+import Link from "next/link";
 import { TypographyH1, TypographyP } from "@/components/typography";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-start p-24">
+    <main className="flex min-h-screen flex-col items-start p-24 gap-12">
       <TypographyH1>nozomemein</TypographyH1>
-      <TypographyP>
-        Hello, I&apos;m nozomemein, a passionate software engineer.
-      </TypographyP>
-      <TypographyP>
-        I enjoy building solutions and continuously learning to improve my craft.
-      </TypographyP>
+      <div>
+        <TypographyP>
+          Hello, I&apos;m nozomemein, a passionate software engineer.
+        </TypographyP>
+        <TypographyP>
+          I enjoy building solutions and continuously learning to improve my craft.
+        </TypographyP>
+      </div>
+      <div className="flex flex-row item-start gap-4">
+        <Link href="https://x.com/nozomi9821" target="_blank" className="text-primay underline-offset-4 hover:underline text-sm font-medium inline-flex items-center">
+          Twitter/X
+        </Link>
+        <Link href="https://github.com/nozomemein" target="_blank" className="text-primay underline-offset-4 hover:underline text-sm font-medium inline-flex items-center">
+          GitHub
+        </Link>
+      </div>
     </main>
   );
 }
