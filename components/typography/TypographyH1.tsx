817c7f420914fa7b6d3024537e010489273a76d5
nozomemein <b8yukifsukeo999n@gmail.com>
2024-06-08 09:57:26 +0900

    ✨add typographys & main page

---
+++ components/typography/TypographyH1.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from 'react'
+type Props = {
+  children: ReactNode
+}
+export const TypographyH1= ({ children }: Props) => {
+  return (
+    <h1 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+      {children}
+    </h1>
+  )
+}
---
+++ components/typography/index.tsx
@@ -0,0 +1,4 @@
+import { TypographyH1 } from "./TypographyH1";
+import { TypographyP } from "./TypographyP";
+
+export { TypographyH1, TypographyP }
